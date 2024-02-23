193d3a9e0bd6197affa624d48ef67b3ecd37b274
qkrdudqnsla <https://github.com/qkrdudqnsla>
2024-02-23 16:15:42 +0900

    Main.java

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Main {
+    public static void main(String[] args) {
+/*        String UuId = UUID.randomUUID().toString().replaceAll("-", "");
+        System.out.println("유효아이디 : "  + UuId);*/
+/*        //99단;
+        Gugudan gugudan = new Gugudan();
+        gugudan.call();*/
+
+/*       //super();
+        childArg.printName();*/
+
+/*        //static 예제1
+        Static test = new Static();
+        Static test2 = new Static();
+        test.num++;
+        test.num2++;
+        System.out.println(test.num);
+        System.out.println(test.num2);
+        System.out.println(test2.num);
+        System.out.println(test2.num2);
+
+        //static 예제2
+        //유틸리티 메소드
+        Static.method();
+        Static test3 = new Static();
+        test3.method2();*/
+
+/*        //주사위
+        Jusawi.jusawi();*/
+
+
+        //리턴
+        //ReturnTest.test();
+
+/*        ReturnTest returnTest = new ReturnTest();
+        returnTest.test1();*/
+
+/*        ReturnTest returnTest = new ReturnTest();
+
+        // ReturnTest의 test1 메서드 호출 및 반환된 값을 result 변수에 저장
+        ReturnType result = (ReturnType) returnTest.test1(); // 캐스팅 필요
+
+        // result 객체의 데이터 확인
+        Object data = result.getData();
+        //if (data instanceof ReturnType2) {
+        ReturnType2 returnType2 = (ReturnType2) data;
+        // ReturnType2 객체의 내용 출력
+        System.out.println("Status: " + returnType2.getStatus());
+        System.out.println("Message: " + returnType2.getMessage());*/
+        //}
+
+/*        //for문연습
+        ForTest.fortest();*/
+
+    }
+
+
+
+}
